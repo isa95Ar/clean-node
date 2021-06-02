a942dc8641c088dfa617e3dc1b9ef1477a1b91dc
Isaias Diaz <isaarg2312@gmail.com>
2021-06-02 19:49:16 -0300

    clean node for react native

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     database_user : 'root',
     database_password : '',
     database_name:'backendisp',
-    port : 4000,
+    port : 8080,
     app_name: ''
 };
 
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const { config } = require('./config/config'); //objeto con info estática sobre la base de datos
-const api = require('./src/api'); //establece respuestas a '/restaurants' y a '/restaurants/:id?/:name?'
+//const api = require('./src/api'); //establece respuestas a '/restaurants' y a '/restaurants/:id?/:name?'
 
-require("./database/connection"); //hace disponibles las tablas User, Role, Category y Restaurant
+//require("./database/connection"); //hace disponibles las tablas User, Role, Category y Restaurant
 
 const express = require('express');
 const app = express();
 
-app.use('/api', api); //establece respuestas a a '/api/restaurants' y a '/api/restaurants/:id?/:name?'
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//app.use('/api', api); //establece respuestas a a '/api/restaurants' y a '/api/restaurants/:id?/:name?'
+app.get('/',(req,res)=>{
+    res.json({title:'Hello from api'});
+});
+
+app.post('/create/user',(req,res) => {
+  const data = req.body;
+  console.log(data);
+});
 app.listen(config.port, () => {
   console.log('el servidor esta corriendo =) ');
 }); //mantiene escucha en el puerto establecido en config
